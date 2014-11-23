59a4a6b35b8271ec32eb389b086d6dbdb242579d
WMuruet <wmuruet.inv.clinica@gmail.com>
2014-11-23 21:58:28 +0000

    Data Scientist Assignment2
    Programming Assignment 2
    By Walter Muruet

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## these functions create a special matrix object that can be cached. it the computes the inverse matrix
+## but they will only compute the inverse matrix if it is not already in the cache. 
+## If the inverse matrix is already in the cache, cacheSolve will just retrive it
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object. The inverse of this object can be "cached"
+## it returns a list that will be used as input by the cacheSolve function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inversematrix = NULL
+        setmatrix = function(y) {
+                x <<- y
+                inversematrix <<- NULL
+        }
+        getmatrix = function() x
+        setinversematrix = function(inverse) inversematrix <<- inverse 
+        getinversematrix = function() inversematrix
+        list(set=setmatrix, get=getmatrix, setinv=setinversematrix, getinv=getinversematrix)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse matrix. it computes the inverse matrix if it has not already been calculated. Otherwise, it retrives
+##it from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+            
+        inversematrix = x$getinv()
+        
+        if (!is.null(inversematrix)){
+             print("Retriving Cache...please wait")
+             message(inversematrix)   
+             return(inversematrix)
+               
+                
+        }
+        
+        else { 
+        mat.data = x$get()
+        inversematrix = solve(mat.data, ...)
+        x$setinv(inversematrix)
+        print("Computing...please wait")
+        message(inversematrix)
+        return(inversematrix)
+         }
 }
+                
